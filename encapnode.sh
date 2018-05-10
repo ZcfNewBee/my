f5f3e728c2c71b276aead7d210e62fb5be91b77a
ZcfNewBee <37317820+ZcfNewBee@users.noreply.github.com>
2018-05-10 10:20:18 +0800

    Delete encapnode.sh

--- encapnode.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-vppctl create host-interface name vpp0
-vppctl create host-interface name vpp1
-vppctl set interface state host-vpp0 up
-vppctl set interface state host-vpp1 up
-vppctl set interface ip address host-vpp0 2001::2/64
-vppctl set interface ip address host-vpp1 2001:1::2/64
-vppctl ip route add ::/0 via 2001:1::3
