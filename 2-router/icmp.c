27cdfe5263bd6c08e04bc19ab5198bcf0529d8a6
ZcfNewBee <37317820+ZcfNewBee@users.noreply.github.com>
2019-06-03 09:55:32 +0800

    Add files via upload

--- 2-router/icmp.c
+++ 2-router/icmp.c
@@ -11,50 +11,76 @@
 // send icmp packet
 void icmp_send_packet(const char *in_pkt, int len, u8 type, u8 code)
 {
-    // 获取输入的ip头
-    struct iphdr *in_ip = packet_to_ip_hdr(in_pkt);
-    u32 out_daddr = ntohl(in_ip->saddr);
-    u32 out_saddr = longest_prefix_match(out_daddr)->iface->ip;
-
-    long tot_size = 0;
-    char *out_pkt = NULL;
-
-    //申请数据包空间
+    
     if (type != ICMP_ECHOREPLY)
-        tot_size = ETHER_HDR_SIZE + IP_BASE_HDR_SIZE + ICMP_HDR_SIZE + IP_HDR_SIZE(in_ip) + 8;
-    else
-        tot_size = len - IP_HDR_SIZE(in_ip) + IP_BASE_HDR_SIZE;
+	{
+		 struct iphdr *in_ip = packet_to_ip_hdr(in_pkt);
+    	u32 daddr = ntohl(in_ip->saddr);
+    	u32 saddr = longest_prefix_match(daddr)->iface->ip;
 
-    out_pkt = (char*) malloc(tot_size);
+    	long total_ip_len = 0;
+		total_ip_len = ETHER_HDR_SIZE + IP_BASE_HDR_SIZE + ICMP_HDR_SIZE + IP_HDR_SIZE(in_ip) + 8;
+		char* packet = (char*) malloc(total_ip_len);
 
-    struct icmphdr *icmp = (struct icmphdr *)(out_pkt + ETHER_HDR_SIZE + IP_BASE_HDR_SIZE);
+    	struct icmphdr *icmp_hdr = (struct icmphdr *)(packet + ETHER_HDR_SIZE + IP_BASE_HDR_SIZE);
 
-    struct ether_header *eh = (struct ether_header *)out_pkt;
-    eh->ether_type = htons(ETH_P_IP);
+    	struct ether_header *eh = (struct ether_header *)packet;
+    	eh->ether_type = htons(ETH_P_IP);
 
-    struct iphdr *out_ip_hdr = packet_to_ip_hdr(out_pkt);
-
-    ip_init_hdr(out_ip_hdr,
-                out_saddr,
-                out_daddr,
-                tot_size - ETHER_HDR_SIZE,
+    	struct iphdr *ip_hdr = packet_to_ip_hdr(packet);
+		ip_init_hdr(ip_hdr,
+                saddr,
+                daddr,
+                total_ip_len - ETHER_HDR_SIZE,
                 IPPROTO_ICMP);
 
    
 
-    memset(icmp, 0, ICMP_HDR_SIZE);
-    icmp->code = code;
-    icmp->type = type;
-
-    if (type != ICMP_ECHOREPLY)
-        memcpy(((char*)icmp + ICMP_HDR_SIZE),
+   		memset(icmp_hdr, 0, ICMP_HDR_SIZE);
+    	icmp_hdr->code = code;
+    	icmp_hdr->type = type;
+		memcpy(((char*)icmp_hdr + ICMP_HDR_SIZE),
                (char*)in_ip,
                IP_HDR_SIZE(in_ip) + 8);
+		icmp_hdr->checksum =
+        icmp_checksum(icmp_hdr, total_ip_len-ETHER_HDR_SIZE-IP_BASE_HDR_SIZE);
+    	ip_send_packet(packet, total_ip_len);
+	}
+        
     else
-        memcpy(((char*)icmp) + ICMP_HDR_SIZE - 4,
+	{
+		// 获取输入的ip头
+    	struct iphdr *in_ip = packet_to_ip_hdr(in_pkt);
+    	u32 daddr = ntohl(in_ip->saddr);
+    	u32 saddr = longest_prefix_match(daddr)->iface->ip;
+
+    	long total_ip_len = 0;
+		total_ip_len = len - IP_HDR_SIZE(in_ip) + IP_BASE_HDR_SIZE;
+		char* packet = (char*) malloc(total_ip_len);
+
+    	struct icmphdr *icmp_hdr = (struct icmphdr *)(packet + ETHER_HDR_SIZE + IP_BASE_HDR_SIZE);
+
+    	struct ether_header *eh = (struct ether_header *)packet;
+    	eh->ether_type = htons(ETH_P_IP);
+
+    	struct iphdr *ip_hdr = packet_to_ip_hdr(packet);
+
+    	ip_init_hdr(ip_hdr,
+                saddr,
+                daddr,
+                total_ip_len - ETHER_HDR_SIZE,
+                IPPROTO_ICMP);
+
+   
+
+    	memset(icmp_hdr, 0, ICMP_HDR_SIZE);
+    	icmp_hdr->code = code;
+    	icmp_hdr->type = type;
+		memcpy(((char*)icmp_hdr) + ICMP_HDR_SIZE - 4,
                in_pkt + ETHER_HDR_SIZE + IP_HDR_SIZE(in_ip) + 4,
                len - ETHER_HDR_SIZE - IP_HDR_SIZE(in_ip) - 4);
-    icmp->checksum =
-        icmp_checksum(icmp, tot_size-ETHER_HDR_SIZE-IP_BASE_HDR_SIZE);
-    ip_send_packet(out_pkt, tot_size);
+		icmp_hdr->checksum =
+        icmp_checksum(icmp_hdr, total_ip_len-ETHER_HDR_SIZE-IP_BASE_HDR_SIZE);
+    	ip_send_packet(packet, total_ip_len);
+	}  
 }
